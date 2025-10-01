f93eeed34c1f40ea99a9ff1251f901b9734cde4c
SangWon7242 <gotkdqja@gmail.com>
2025-10-01 21:29:22 +0900

    백 : 게시물 리스트를 프론트로 전달

--- backend/src/main/java/com/sbs/blogfy/domain/post/controller/PostController.java
+++ backend/src/main/java/com/sbs/blogfy/domain/post/controller/PostController.java
@@ -1,10 +1,13 @@
 package com.sbs.blogfy.domain.post.controller;
 
+import com.sbs.blogfy.domain.post.entity.Post;
 import com.sbs.blogfy.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +16,7 @@ public class PostController {
   private final PostService postService;
 
   @GetMapping("/list")
-  public String list() {
-    return "list";
+  public List<Post> showList() {
+    return postService.getPosts();
   }
 }
